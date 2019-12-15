02ba2a7650e960519fb631c865f11f5def84fbdd
Washington <washingtonlgarcia@gmail.com>
2019-12-15 17:08:33 -0300

    Criando loader de models

---
+++ src/database/index.js
@@ -0,0 +1,19 @@
+// Onde feito a conexão com banco de dados
+import Sequelize from 'sequelize';
+import User from '../app/models/User';
+import databaseConfig from '../config/database';
+
+const models = [User];
+class DataBase {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new DataBase();
