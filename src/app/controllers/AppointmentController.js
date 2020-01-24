f4abe3a2dc5708f64633670134df40fe09a30bde
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2020-01-24 20:22:43 -0300

    Validações de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -9,7 +10,7 @@ class AppointmentController {
       date: Yup.date().required(),
     });
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails' });
+      return res.status(400).json({ error: 'Validation fails.' });
     }
     const { provider_id, date } = req.body;
 
@@ -20,7 +21,27 @@ class AppointmentController {
     if (!checkIsProvider) {
       return res
         .status(401)
-        .json({ error: 'You can only appointments with providers' });
+        .json({ error: 'You can only appointments with providers.' });
+    }
+
+    // Check for past dates
+    const hourStart = startOfHour(parseISO(date));
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted.' });
+    }
+
+    // Check date availability
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available.' });
     }
     const appointment = await Appointment.create({
       user_id: req.userId,
