dd5b85dae396a08eeec53e47737413031b2d8ece
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2020-01-25 01:16:54 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4fdf26d84d3a55',
+    pass: '48119e16a71ef5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
