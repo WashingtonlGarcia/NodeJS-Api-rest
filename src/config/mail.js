73467623f464b25205675e56b7d71a282c9693d4
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2020-01-25 19:48:12 -0300

    Variáveis ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '4fdf26d84d3a55',
-    pass: '48119e16a71ef5',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
